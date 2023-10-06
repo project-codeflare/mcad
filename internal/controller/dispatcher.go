76c869416057aa653e47b606018ae98b05c03ee2
Olivier Tardieu <tardieu@us.ibm.com>
2023-10-06 13:33:02 -0400

    Fix dispatchedNanos

--- internal/controller/dispatcher.go
+++ internal/controller/dispatcher.go
@@ -140,10 +140,8 @@ func (r *Dispatcher) update(ctx context.Context, appWrapper *mcadv1beta1.AppWrap
 	if (appWrapper.Spec.DispatcherStatus.Phase == mcadv1beta1.Dispatching ||
 		appWrapper.Spec.DispatcherStatus.Phase == mcadv1beta1.Running ||
 		appWrapper.Spec.DispatcherStatus.Phase == mcadv1beta1.Requeuing) &&
-		phase == mcadv1beta1.Failed ||
-		phase == mcadv1beta1.Succeeded ||
-		phase == mcadv1beta1.Queued {
-		appWrapper.Spec.DispatcherStatus.DispatchedNanos += time.Since(appWrapper.Spec.DispatcherStatus.LastDispatchingTime.Time).Nanoseconds()
+		(phase == mcadv1beta1.Failed || phase == mcadv1beta1.Succeeded || phase == mcadv1beta1.Queued) {
+		appWrapper.Spec.DispatcherStatus.DispatchedNanos += int64(time.Since(appWrapper.Spec.DispatcherStatus.LastDispatchingTime.Time).Seconds()) * 1000000000
 	}
 	appWrapper.Spec.DispatcherStatus.Phase = phase
 	if err := r.Update(ctx, appWrapper); err != nil {
